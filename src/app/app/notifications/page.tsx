c12a302bc7e6c120256996033cf797bf0f940a9c
AustinRexi <Austinaminu@gmail.com>
2025-07-25 07:49:43 +0100

    fixed some bugs to enhance deployment

--- src/app/app/notifications/page.tsx
+++ src/app/app/notifications/page.tsx
@@ -9,13 +9,12 @@ const ToggleSwitch = ({ checked = false }) => (
     <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-500"></div>
   </label>
 );
-
+interface NotificationSettingsProps {
+  onClose?: () => void;
+}
 export default function NotificationSettings({
   onClose,
-}: {
-  onClose?: () => void;
-}) {
-  // Data structure for notification settings
+}: NotificationSettingsProps) {
   const notificationGroups = [
     {
       title: "Global Notifications",
